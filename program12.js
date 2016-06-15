db35f105b7dfdd552c921d61f3128ce233b41d28
Trang <t-mai@amida.co.jp>
2016-06-15 18:12:44 +0900

    Solution for challenge 12

---
+++ program12.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var port = process.argv[2];
+var map = require('through2-map');
+
+var server = http.createServer(function (req, res) {
+    if(req.method == 'POST') {
+
+        req.pipe(map(function (chunk) {
+                return chunk.toString().toUpperCase();
+            })).pipe(res);
+
+        req.on('error', function (error) {
+            res.end(error);
+        });
+    }
+});
+server.listen(port);
