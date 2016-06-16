613706f15a98e717bae11e048c9ca12250976596
Trang <t-mai@amida.co.jp>
2016-06-16 21:28:21 +0900

    Excercise 11

--- program11.js
+++ program11.js
@@ -14,17 +14,3 @@ var server = http.createServer(function (request, response) {
     });
 });
 server.listen(port);
-
-/** Official solution
- *var http = require('http')
-     var fs = require('fs')
- 
-     var server = http.createServer(function (req, res) {
-       res.writeHead(200, { 'content-type': 'text/plain' })
- 
-       fs.createReadStream(process.argv[3]).pipe(res)
-     })
- 
-     server.listen(Number(process.argv[2])) 
- *
- */
