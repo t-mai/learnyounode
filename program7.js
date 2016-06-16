cbf0053f35ae78235d7598a359e51febf32652ed
Trang <t-mai@amida.co.jp>
2016-06-16 21:02:53 +0900

    Excercise 7

---
+++ program7.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var url = process.argv[2];
+
+http.get(url, function (response) {
+    response.setEncoding("utf-8");
+    response.on("data", function (data) {
+        console.log(data);
+    response.on("error", console.error);
+    });
+});
