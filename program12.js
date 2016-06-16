3afbfb692c4c17db4d461dfecc54b9a7628a43f8
Trang <t-mai@amida.co.jp>
2016-06-16 21:28:35 +0900

    Excercise 12

--- program12.js
+++ program12.js
@@ -4,7 +4,6 @@ var map = require('through2-map');
 
 var server = http.createServer(function (req, res) {
     if(req.method == 'POST') {
-
         req.pipe(map(function (chunk) {
                 return chunk.toString().toUpperCase();
             })).pipe(res);
