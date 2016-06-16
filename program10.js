0ba7b4bfc571d848542eca01c84f1484404fa48c
Trang <t-mai@amida.co.jp>
2016-06-16 21:26:33 +0900

    Excercise 10

--- program10.js
+++ program10.js
@@ -7,28 +7,3 @@ var server = net.createServer(function (socket) {
     return socket.end(strftime('%Y-%m-%d %H:%M' + '\n'));
 });
 server.listen(port);
-
-//Official solution
-/*
-var net = require('net')  
-       
-     function zeroFill(i) {  
-       return (i < 10 ? '0' : '') + i  
-     }  
-       
-     function now () {  
-       var d = new Date()  
-       return d.getFullYear() + '-'  
-         + zeroFill(d.getMonth() + 1) + '-'  
-         + zeroFill(d.getDate()) + ' '  
-         + zeroFill(d.getHours()) + ':'  
-         + zeroFill(d.getMinutes())  
-     }  
-       
-     var server = net.createServer(function (socket) {  
-       socket.end(now() + '\n')  
-     })  
-       
-     server.listen(Number(process.argv[2]))
-
-*/
